04af54e61c19b7e131e7305c0f5d38de5cdc7f87
RVSher <RVShershnev@yandex.ru>
2019-10-16 01:30:06 +0300

    Изменен Hello World.

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,7 +7,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello GitLab!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
